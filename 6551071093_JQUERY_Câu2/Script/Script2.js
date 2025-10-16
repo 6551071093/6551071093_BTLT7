60f401db5f4192c45244caa9a1e8349dcc9c62e8
6551071093 <6551071093@st.utc2.edu.vn>
2025-10-16 19:04:55 +0700

    Delete 6551071093_JQUERY_Câu2 directory

--- 6551071093_JQUERY_Câu2/Script/Script2.js
+++
@@ -1,6 +0,0 @@
-function getFormvalue(){
-    var firstname = $('form :input[name="fname"]').val();
-    var lastname = $('form :input[name="lname"]').val();
-    alert("firstname ="+firstname+", lastname ="+lastname);
-    return false;
-}
